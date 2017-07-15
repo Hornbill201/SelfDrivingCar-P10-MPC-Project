6c309161ded4f718075a8d0d3bd61135bfa1e276
Chao Wang <kevin.mercury52@gmail.com>
2017-07-15 15:22:38 -0700

    Compute the Cost and Set up the Constraints

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -6,8 +6,8 @@
 using CppAD::AD;
 
 // TODO: Set the timestep length and duration
-size_t N = 0;
-double dt = 0;
+size_t N = 10;
+double dt = 0.1;
 
 // This value assumes the model presented in the classroom is used.
 //
@@ -21,6 +21,22 @@ double dt = 0;
 // This is the length from front to CoG that has a similar radius.
 const double Lf = 2.67;
 
+
+double des_v = 200;
+double des_cte = 0;
+double des_epsi = 0;
+
+size_t x_start = 0;
+size_t y_start = x_start + N;
+size_t psi_start = y_start + N;
+size_t v_start = psi_start + N;
+size_t cte_start = v_start + N;
+size_t espi_start = cte_start + N;
+size_t delta_start = espi_start + N;
+size_t a_start = delta_start + N - 1;
+
+
+
 class FG_eval {
  public:
   // Fitted polynomial coefficients
@@ -33,6 +49,82 @@ class FG_eval {
     // `fg` a vector of the cost constraints, `vars` is a vector of variable values (state & actuators)
     // NOTE: You'll probably go back and forth between this function and
     // the Solver function below.
+
+    fg[0] = 0;
+
+    // weights of the variables
+    double v_w = 1;
+    double cte_w = 5000;
+    double epsi_w = 5000;
+    double delta_w = 100;
+    double a_w = 1;
+    double delta_change_w = 1000;
+    double a_change_w = 1;
+
+    // cost on state (velocity error, cte, heading error)
+    for (int t = 0; t < N; t++)
+    {
+      fg[0] += v_w * CppAD::pow(vars[v_start+t] - des_v, 2);
+      fg[0] += cte_w * CppAD::pow(vars[cte_start+t] - des_cte, 2);
+      fg[0] += epsi_w * CppAD::pow(vars[espi_start+t] - des_epsi, 2)
+    }
+
+    // cost of make turn and acceleration smoother
+    for (int t=0; t<N-1; t++)
+    {
+      fg[0] += delta_w * CppAD::pow(vars[delta_start+t],2);
+      fg[0] += a_w * CppAD::pow(vars[a_start+t],2);
+    }
+
+    // cost of change based on last change to make sequential actions smoother
+    for (int t=0; t<N-2; t++)
+    {
+      fg[0] += delta_change_w * CppAD::pow(vars[delta_start+t+1] - vars[delta_start+t], 2);
+      fg[0] += a_change_w * CppAD::pow(vars[a_start+t+1] - vars[a_start+t],2);
+    }
+
+    // Set up constraints. Add 1 to starting indices due to cost being at index 0 of fg
+    fg[1+x_start] = vars[x_start];
+    fg[1+y_start] = vars[y_start];
+    fg[1+psi_start] = vars[psi_start];
+    fg[1+v_start] = vars[v_start];
+    fg[1+cte_start] = vars[cte_start];
+    fg[1+epsi_start] = vars[epsi_start];
+
+    for (int t=0; t<N-1; t++) {
+      // State at timestep t
+      AD<double> x0 = vars[x_start+t];
+      AD<double> y0 = vars[y_start+t];
+      AD<double> psi0 = vars[psi_start+t];
+      AD<double> v0 = vars[v_start+t];
+      AD<double> cte0 = vars[cte_start+t];
+      AD<double> epsi0 = vars[epsi_start+t];
+      
+      // Control inputs at timestep t
+      AD<double> delta0 = vars[delta_start+t];
+      AD<double> a0 = vars[a_start+t];
+
+      // State at timestep t+1
+      AD<double> x1 = vars[x_start+t+1];
+      AD<double> y1 = vars[y_start+t+1];
+      AD<double> psi1 = vars[psi_start+t+1];
+      AD<double> v1 = vars[v_start+t+1];
+      AD<double> cte1 = vars[cte_start+t+1];
+      AD<double> epsi1 = vars[epsi_start+t+1];
+
+      // Calculate f(x) and desired orientation
+      // Desired orientation is the tangential angle of the derivitive of the polynomial
+      AD<double> f0 = coeffs[3] * pow(x0,3) + coeffs[2] * pow(x0,2) + coeffs[1] * x0 + coeffs[0];
+      AD<double> psi_des0 = CppAD::atan(3 * coeffs[3] * pow(x0,2) + 2 * coeffs[2] * x0 + coeffs[1]);
+
+      // Set up the model constraints (should be equal to 0)
+      fg[2+x_start+t] = x1 - (x0 + v0 * CppAD::cos(psi0) * dt);
+      fg[2+y_start+t] = y1 - (y0 + v0 * CppAD::sin(psi0) * dt);
+      fg[2+psi_start+t] = psi1 - (psi0 - (v0/Lf) * delta0 * dt);
+      fg[2+v_start+t] = v1 - (v0 + a0 * dt);
+      fg[2+cte_start+t] = cte1 - ((f0 - y0) + (v0 * CppAD::sin(epsi0) * dt));
+      fg[2+epsi_start+t] = epsi1 - ((psi0 - psi_des0) - (v0/Lf) * delta0 * dt);
+    }
   }
 };
 
